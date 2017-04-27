dc1154759d57fb1ea7a114c8cb468ec9f7341e05
ScottBrenden <scott.b.brenden@gmail.com>
2017-04-26 17:37:00 -0700

    setup and build of forEach

---
+++ lab-scott/lib/list.js
@@ -0,0 +1,43 @@
+'ues strict';
+
+const List = module.exports = function() {
+  for(let key in arguments) {
+    this[key] = arguments[key];
+  }
+  this.length = arguments.length;
+};
+
+// O(n)
+List.prototype.copy = function() {
+  let result = new List();
+  for(let key in this) {
+    result[key] = this[key];
+  }
+  return result;
+};
+
+// O(n)
+List.prototype.push = function(value) {
+  let result = this.copy();
+  result[result.length++] = value;
+  return result;
+};
+
+// O(n)
+List.prototype.pop = function() {
+  let result = this.copy();
+  delete result[--result.length];
+
+  return {
+    value: this[this.length - 1],
+    list: result,
+  };
+};
+
+List.prototype.forEach = function(cb){
+  for (let i=0; i<this.length; i++){
+    cb(this[i], i, this);
+  }
+};
+
+List.prototype.filter = function()
