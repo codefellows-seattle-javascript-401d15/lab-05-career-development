4c87eb7b13dfe735786413302d2ad5fd18ce39ad
Abigail White <abigailwhite213@gmail.com>
2017-04-28 10:20:36 -0700

    tests written

--- lab-abigail/lib/prototype.js
+++ lab-abigail/lib/prototype.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const List = module.exports = function() {
-  for(let keys in arguments) {
+  for(let key in arguments) {
     this[key] = arguments[key];
   }
   this.length = arguments.length;
 };
 
 // O(n)
-List.prototype.copy = function() {
+List.prototype.testCopy = function() {
   let result = new List();
   for(let key in this) {
     result[key] = this[key];
@@ -17,15 +17,15 @@ List.prototype.copy = function() {
 };
 
 // O(n)
-List.prototype.push = function(value) {
-  let result = this.copy();
+List.prototype.testPush = function(value) {
+  let result = this.testCopy();
   result[result.length++] = value;
   return result;
 };
 
 // O(n)
-List.prototype.pop = function() {
-  let result = this.copy();
+List.prototype.testPop = function() {
+  let result = this.testCopy();
   delete result[--result.length];
 
   return {
@@ -35,43 +35,45 @@ List.prototype.pop = function() {
 };
 
 //O(n)
-List.prototype.forEach = function(callback) {
+List.prototype.testForEach = function(callback) {
 
   for(let i = 0; i < this.length; i++) {
     callback(this[i], i, this);
   }
 };
 
+
 //O(n)
-List.prototype.filter = function(callback) {
+List.prototype.testFilter = function(callback) {
   let result = [];
 
   for(let i = 0; i < this.length; i++) {
     if (callback(this[i], i, this)) {
-      result.push[i];
+      result.testPush(this[i]);
     }
     return result;
   }
 };
 
 //
-List.prototype.map = function(callback) {
+List.prototype.testMap = function(callback) {
   let result = [];
 
   for (let i = 0; i < List.length; i++) {
     callback(this[i], i, this);
-    result.push[i];
+    result.testPush(this[i]);
   }
   return result;
 };
 
 //
-List.prototype.reduce = function(callback) {
+List.prototype.testReduce = function(callback) {
   let result = 0;
 
   for (let i = 0; i < List.length; i++) {
     this[i] += result;
     callback(this[i], i, this);
   }
+  console.log(result);
   return result;
 };
--- lab-abigail/test/test.js
+++ lab-abigail/test/test.js
@@ -1,14 +1,181 @@
 'use strict';
 
-const fs = require('fs');
 const expect = require('chai').expect;
-const prototype = require('../lib/prototype');
+const List = require('../lib/prototype');
+
+let test = new List();
 
 describe('reduce method', function() {
-  let List = [1, 2, 3, 4, 5, 6];
 
-  it('should produce 21', function(done) {
-    expect(List.reduce()).to.be.equal(21);
+  let reduceList = test.testPush(2);
+  let testReduceValue = reduceList.testReduce();
+
+  it('has a value of 2', function(done) {
+    expect(reduceList['0']).to.equal(2);
     done();
   });
+
+  it('should exist', function(done) {
+    expect(testReduceValue).to.exist;
+    done();
+  });
+
+  it('should be a number', function(done) {
+    expect(testReduceValue).to.be.a('number');
+    done();
+  });
+});
+
+describe('pop method', function() {
+
+  let popList = test.testPush(2);
+  let testPopValue = popList.testPop();
+
+  it('should have length of 0', function(done) {
+    expect(testPopValue.list.length).to.equal(0);
+    done();
+  });
+
+  it('should have a testPop value of 2', function(done) {
+    expect(testPopValue.value).to.equal(2);
+    done();
+  });
+
+  it('should be a number', function(done) {
+    expect(testPopValue.value).to.be.a('number');
+    done();
+  });
+
+});
+
+describe('map method', function() {
+  let mapList = test.testPush(2);
+  let testMapValue = mapList.testMap(function(ele) {
+    console.log(`ELE ${ele}`);
+  });
+
+  it('has a value of 2', function(done) {
+    console.log(mapList);
+    expect(mapList['0']).to.equal(2);
+    done();
+  });
+
+  it('has a length of 1', function(done) {
+    expect(mapList.length).to.equal(1);
+    done();
+  });
+
+  it('Test Value should be an array', function(done) {
+    expect(testMapValue).to.be.a('array');
+    done();
+  });
+
+  it('Test Value should have length of 0', function(done) {
+    expect(testMapValue).have.lengthOf(0);
+    done();
+  });
+});
+
+describe('filter method', function() {
+  let filterList = test.testPush(2);
+  let testFilterValue = filterList.testFilter(function(ele) {
+    console.log(`ELE ${ele}`);
+  });
+
+  it('has a value of 2', function(done) {
+    expect(filterList['0']).to.equal(2);
+    done();
+  });
+
+  it('should not exist', function(done) {
+    expect(testFilterValue).to.be.an('array');
+    done();
+  });
+
+  it('should be a number', function(done) {
+    expect(testFilterValue).to.be.empty;
+    done();
+  });
+
+});
+
+describe('forEach method', function() {
+  let forEachList = test.testPush(2);
+  let testForEachValue = forEachList.testForEach(function(ele) {
+    console.log(`ELE ${ele}`);
+  });
+
+  it('has a value of 2', function(done) {
+    expect(forEachList['0']).to.equal(2);
+    done();
+  });
+
+  it('should not exist', function(done) {
+    expect(testForEachValue).to.not.exist;
+    done();
+  });
+
+  it('should be a number', function(done) {
+    expect(testForEachValue).to.be.undefined;
+    done();
+  });
+
+});
+
+
+describe('push method', function() {
+  let pushList = test.testPush(2);
+  let pushList2 = pushList.testPush(3);
+
+  it('Original List to have a value of 2', function(done) {
+    expect(pushList['0']).to.equal(2);
+    done();
+  });
+
+  it('should exist', function(done) {
+    expect(pushList2).to.exist;
+    done();
+  });
+
+  it('should be a number', function(done) {
+    expect(pushList2).to.not.deep.equal(pushList);
+    done();
+  });
+
+  it('', function(done) {
+    expect(pushList2['1']).to.equal(3);
+  })
+
+});
+
+describe('copy method', function() {
+  let copyList = test.testPush(2);
+  let testCopyValue = copyList.testCopy();
+
+  it('has a value of 2', function(done) {
+    expect(copyList['0']).to.equal(2);
+    console.log(testCopyValue);
+    done();
+  });
+
+  it('Test Copy to have a value of 2', function(done) {
+    expect(testCopyValue['0']).to.equal(2);
+    done();
+  });
+
+  it('Test Copy should match original', function(done) {
+    expect(testCopyValue).to.deep.equal(copyList);
+    done();
+  })
+
+  it('Test Copy should exist', function(done) {
+    expect(testCopyValue).to.exist;
+    done();
+  });
+
+  it('should be a number', function(done) {
+    expect(testCopyValue).to.be.an('object');
+    done();
+  });
+
 });
