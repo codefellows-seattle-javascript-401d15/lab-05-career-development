fd0674fe0310251abfad9aacfa91ed0f3bd7e730
galang pham <phamgalang@gmail.com>
2017-04-28 13:14:36 -0700

    turn in, not finished

--- lab-glen/lib/index.js
+++ lab-glen/lib/index.js
@@ -1,48 +1,53 @@
 'use strict';
 
-function List() {
-  for(let keys in arguments) {
+const List = module.exports = function () {
+  for(let key in arguments) {
     this[key] = arguments[key]
   }
   this.length = arguments.length
 }
 
 //O (n)
-List.prototype.copy = function () {
-  let results = new List()
-  for(let key in this) {
-    results[key] = this[key]
-  }
+List.prototype.copy = function (source) {
+  let results = source
   return results
 }
 
 //O(n)
 List.prototype.push = function (value) {
-  let result = this.copy()
-  result[result.length++] = value
-  return result
-}
+  let result = this.copy();
+  result[result.length++] = value;
+  return result;
+};
 
 //O(n)
-List.protoytype.pop = function () {
-  let result = this.copy()
-  delete result[--result.length]
-
-  return {
-    value: this[this.length -1],
-    list: result
-  }
-}
+// List.protoytype.pop = function () {
+//   let result = this.copy();
+//   delete result[--result.length];
+//
+//   return {
+//     value: this[this.length -1],
+//     list: result,
+//   };
+// };
 
 // O(n)
 List.prototype.forEach = function (callback) {
   for (var i = 0; i < this.length; i++) {
     callback(this[i], i, this)
+    console.log(this)
   }
 }
 
 List.prototype.filter = function (callback) {
   let result = this.copy()
+  for (var i = 0; i < this.length; i++) {
+    callback()
+    if(this[i] === value) {
+      result.push(this[i])
+    }
+  }
+  return result
 }
 
 List.prototype.map = function (callback) {
@@ -54,5 +59,7 @@ List.prototype.map = function (callback) {
 }
 
 List.prototype.reduce = function (callback) {
-
+  for (var i = 0; i < this.length; i++) {
+    this[i]
+  }
 }
