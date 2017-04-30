44bb26a8f17aa6a5208173e3a6d58584434fdd83
michaeltreat <michael.leon.treat@gmail.com>
2017-04-29 21:31:08 -0700

    reached the point where the demos are at. Thoughts after playing around with it in node - These are all pure functions, since our first list method is a copy. This means we can copy a dataset without inflicting side effects on that set. I also enjoyed using bracket notation much more and seeing why we would use it over dot notation.  Pop had an interesting effect of returing a new object with two props, one that mentions what was popped and one that gives the new popped object list.

--- lab-miketreat/lib/lists.js
+++ lab-miketreat/lib/lists.js
@@ -1,1 +1,38 @@
-'use-strict';
+'use strict';
+
+const List = module.exports = function () {
+  for (let key in arguments) {
+    this[key]= arguments[key];
+    console.log(key);
+    console.log(this[key] = arguments[key]);
+  }
+  this.length = arguments.length;
+};
+
+// O(n) because we are iterating over n items.
+List.prototype.copy = function () {
+  let result = new List();
+  for(let key in this){
+    result[key] = this[key];
+  }
+  return result;
+};
+
+// O(n) because we are just assigning the return of this.copy() to result and then adding our new parameter value to the end of that that array.
+List.prototype.push = function(value){
+  let result = this.copy();
+  result[result.length++] = value;
+  return result;
+};
+
+
+// O(n) because like in .push we are just assigning the return of this.copy and removing a result from the array.
+List.prototype.pop = function(){
+  let result = this.copy();
+  delete result[--result.length];
+
+  return{
+    value: this[this.length - 1],
+    list: result,
+  };
+};
