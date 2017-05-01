ea4c291d312f6870ce7a8f09cfbda16655679893
Brianna <briannaburrows@gmail.com>
2017-04-30 19:31:39 -0700

    added copy test

--- lab-brianna/tests/tests.js
+++ lab-brianna/tests/tests.js
@@ -1,1 +1,14 @@
 'use strict';
+const List = require('../lib/prototypes');
+const chai = require('chai');
+
+describe('test list module prototype methods', function() {
+  describe('#copy', function () {
+    it('should create a copy of the list'), done => {
+      let list = new List(1, 2, 3, 4);
+      let copiedList = list.copy(list);
+      expect(copied).to.equal(list);
+      done();
+    }
+  });
+});
