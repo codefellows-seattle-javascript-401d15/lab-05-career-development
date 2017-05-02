9b4bb5a005bf26789aeee60814688d559b0542d1
Enrique Rico <enriquesantosrico@gmail.com>
2017-05-02 10:08:35 -0700

    mvp without tests

--- lab-enrique/proto.js
+++ lab-enrique/proto.js
@@ -1,10 +1,65 @@
 'use strict';
 
-Array.prototype.forEach = function(data){
+const List = module.exports = function() {
+  for(let key in arguments) {
+    this[key] = arguments[key];
+  }
+  this.length = arguments.length;
+};
+
+//O(n)
+List.prototype.forEach = function(data){
   for(let i = 0; i < this.length; i++) {
     data(this[i]);
-    console.log(data(this[i]));
   }
 };
 
-Array.prototype.forEach();
+//O(n)
+List.prototype.copy = function(source) {
+  let results = source;
+  return results;
+};
+
+//O(n)
+List.prototype.push = function(value) {
+  let result = this.copy(this);
+  result[result.length++] = value;
+  return result;
+};
+
+//O(n)
+List.prototype.pop = function() {
+  let result = this.copy(this);
+  delete result[--result.length];
+
+  return {
+    value: this[this.length -1],
+    list: result,
+  };
+};
+
+List.prototype.filter = function(data){
+  let results = this.copy(this);
+  for(let i = 0; i < this.length; i++){
+    if(this[i] === data) results.push(data);
+  }
+  return results;
+};
+//O(n)
+List.prototype.map = function(callback) {
+  let result = this.copy(this);
+  for (var i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+    result.push(this[i]);
+  }
+  return result;
+};
+
+List.prototype.reduce = function(callback) {
+  let result = 0;
+  for (var i = 0; i < this.length; i++) {
+    result += this[i];
+    callback(this[i], i, this);
+  }
+  return result;
+};
